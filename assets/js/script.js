d75d16ade4ff0af0185ee29854515e8844e65cd1
diegosno <dserranoleal@gmail.com>
2023-04-14 17:43:05 +0200

    move chosenOption variable inside chooseOption function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@ function chooseOption() {
   const options = ["rock", "paper", "scissor"];
   const randomIndex = Math.floor(Math.random() * options.length);
   const chosenOption = options[randomIndex];
+
+  const resultSpan = document.getElementById("result");
+  resultSpan.textContent = chosenOption;
 }
+
 const button = document.getElementById("button");
 button.addEventListener("click", chooseOption);
-
-const resultSpan = document.getElementById("result");
-resultSpan.textContent = chosenOption;
