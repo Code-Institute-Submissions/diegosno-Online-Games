8b468fd0ee7b9551b20c12a40da9cfde1c128c4f
diegosno <dserranoleal@gmail.com>
2023-04-15 01:34:28 +0200

    Update user choice displayed in UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ const cpuChoiceSpan = document.getElementById("cpu-choice");
 for (let i = 0; i < options.length; i++) {
   options[i].addEventListener("click", () => {
     const userChoice = event.target.textContent;
-
+    userChoiceSpan.textContent = userChoice;
     const cpuNumber = Math.floor(Math.random() * cpuOptions.length);
     const cpuChoice = cpuOptions[cpuNumber];
     cpuChoiceSpan.textContent = cpuChoice;
