41aa0768b372b4edd357c8f0b664ca7c9cd58a87
diegosno <dserranoleal@gmail.com>
2023-04-15 01:33:17 +0200

    Get user choice from clicked button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,15 @@ let cpuScore = 0;
 
 const cpuOptions = ["rock", "paper", "scissors"];
 const options = document.getElementsByClassName("button");
+const userChoiceSpan = document.getElementById("user-choice");
 const cpuChoiceSpan = document.getElementById("cpu-choice");
 
 for (let i = 0; i < options.length; i++) {
   options[i].addEventListener("click", () => {
+    const userChoice = event.target.textContent;
+
     const cpuNumber = Math.floor(Math.random() * cpuOptions.length);
     const cpuChoice = cpuOptions[cpuNumber];
+    cpuChoiceSpan.textContent = cpuChoice;
   });
 }
