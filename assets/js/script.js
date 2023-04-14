09bdb53158642d7822a96afb71365a527573baaf
diegosno <dserranoleal@gmail.com>
2023-04-14 17:26:17 +0200

    delete previous even listeners and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,5 @@
-const cpuSelection = document.getElementById("cpuselection");
-const userSelection = document.getElementById("userselection");
-const result = document.getElementById("result");
-const possibleOptions = document.getElementById("button");
-
 function chooseOption() {
   const options = ["rock", "paper", "scissor"];
   const randomIndex = Math.floor(Math.random() * options.length);
   const chosenOption = options[randomIndex];
 }
-
-possibleOptions.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    userChoice = e.target.id;
-  });
-});
-
-let user;
-let cpu;
-let outcome;
