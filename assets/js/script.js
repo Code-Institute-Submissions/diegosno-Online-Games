9ed853fe3ffbaa97d4abdbf67a3cfbd18d50f0b4
diegosno <dserranoleal@gmail.com>
2023-04-15 00:35:40 +0200

    remove for each function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ let cpuScore = 0;
 
 const cpuOptions = ["rock", "paper", "scissors"];
 const options = document.getElementsByClassName("button");
-options.forEach((option) => {
-  option.addEventListener("click", () => {
+ {
     const cpuNumber = Math.floor(Math.random() * cpuOptions.length);
     const cpuChoice = cpuOptions[cpuNumber];
+    console.log(cpuChoice)
   });
 });
