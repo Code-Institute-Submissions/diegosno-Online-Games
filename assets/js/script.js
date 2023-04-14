9540068aab4f469b3d984721a088ea53b69da6dc
diegosno <dserranoleal@gmail.com>
2023-04-14 18:50:52 +0200

    Add scores section

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,12 @@ function chooseOption() {
   const cpuOptionSpan = document.getElementById("cpuoption");
   cpuOptionSpan.textContent = chosenOption;
 }
+
+const buttons = document.querySelectorAll(".button");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    const userOptionSpan = document.getElementById("useroption");
+    userOptionSpan.textContent = this.textContent;
+  });
+}
