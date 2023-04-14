30b4d95fc92fa5681647d220f0e5c696639238c2
diegosno <dserranoleal@gmail.com>
2023-04-14 15:20:15 +0200

    Add main variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+let cpuSelection = document.getElementById("cpuselection");
+let userSelection = document.getElementById("userselection");
+let result = document.getElementById("result");
+let options = document.getElementById("button");
