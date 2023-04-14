487931d0fd23e9d1f8c73d50b45297bdd4a583bf
diegosno <dserranoleal@gmail.com>
2023-04-15 00:40:25 +0200

    Add for loop to iterate through button options

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,10 @@ let cpuScore = 0;
 
 const cpuOptions = ["rock", "paper", "scissors"];
 const options = document.getElementsByClassName("button");
- {
+for (let i = 0; i < options.length; i++) {
     const cpuNumber = Math.floor(Math.random() * cpuOptions.length);
     const cpuChoice = cpuOptions[cpuNumber];
     console.log(cpuChoice)
   });
 });
+
