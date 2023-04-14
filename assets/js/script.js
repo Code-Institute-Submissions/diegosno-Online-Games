e79763c69f4d75327690e39ff7318a3ac584233b
diegosno <dserranoleal@gmail.com>
2023-04-15 00:30:39 +0200

    Add CPU choice selection based on random number index in CPU options array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 let userScore = 0;
 let cpuScore = 0;
 
-
-
-const cpuOptions = ['rock', 'paper', 'scissors];
-const options = document.getElementsByClassName('button');
-options.forEach(option => {option.addEventListener('click') 
-    
+const cpuOptions = ["rock", "paper", "scissors"];
+const options = document.getElementsByClassName("button");
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    const cpuNumber = Math.floor(Math.random() * cpuOptions.length);
+    const cpuChoice = cpuOptions[cpuNumber];
+  });
 });
-const cpuNumber = Math.floor(Math.random() * cpuOptions.length);
