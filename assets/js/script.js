bc3405d16d4de5114e1671122c837b7023151a69
diegosno <dserranoleal@gmail.com>
2023-04-15 00:43:12 +0200

    Add click event listener to the options array.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@ let cpuScore = 0;
 const cpuOptions = ["rock", "paper", "scissors"];
 const options = document.getElementsByClassName("button");
 for (let i = 0; i < options.length; i++) {
+  options[i].addEventListener("click", () => {
     const cpuNumber = Math.floor(Math.random() * cpuOptions.length);
     const cpuChoice = cpuOptions[cpuNumber];
-    console.log(cpuChoice)
+    console.log(cpuChoice);
   });
-});
-
+}
