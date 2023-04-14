4b6e0af2a9e2dd29ef06674a2d1d8b92a7033ff0
diegosno <dserranoleal@gmail.com>
2023-04-14 23:45:57 +0200

    Declare options variable for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let userScore = 0;
 let cpuScore = 0;
 
-const cpuOptions = ["rock", "paper", "scissors"];
+const options = document.getElementsByClassName("button");
 
+const cpuOptions = ["rock", "paper", "scissors"];
 const cpuNumber = Math.floor(Math.random() * cpuOptions.length);
