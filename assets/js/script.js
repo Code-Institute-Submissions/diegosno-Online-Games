61450e70908990d9678711f9ecd0766c25309767
diegosno <dserranoleal@gmail.com>
2023-04-15 00:55:52 +0200

    Add display of CPU choice in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,11 @@ let cpuScore = 0;
 
 const cpuOptions = ["rock", "paper", "scissors"];
 const options = document.getElementsByClassName("button");
+const cpuChoiceSpan = document.getElementById("cpu-choice");
+
 for (let i = 0; i < options.length; i++) {
   options[i].addEventListener("click", () => {
     const cpuNumber = Math.floor(Math.random() * cpuOptions.length);
     const cpuChoice = cpuOptions[cpuNumber];
-    console.log(cpuChoice);
   });
 }
