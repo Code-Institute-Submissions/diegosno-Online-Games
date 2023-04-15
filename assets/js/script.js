c1cdfd86ec37b9c82887e3dd7d19154c8fbd44ac
diegosno <dserranoleal@gmail.com>
2023-04-16 00:57:34 +0200

    Add restartButton variable with event listener for "click"

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,3 +67,6 @@ function restart() {
   userChoiceSpan.textContent = "";
   cpuChoiceSpan.textContent = "";
 }
+
+const restartButton = document.getElementById("restart");
+restartButton.addEventListener("click", restart);
