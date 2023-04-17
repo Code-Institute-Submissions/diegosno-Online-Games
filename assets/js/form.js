72544ad26c30f1baa6fc3e7f92d238a848f38324
diegosno <dserranoleal@gmail.com>
2023-04-17 20:10:08 +0200

    Declare variables by getting elements

---
+++ assets/js/form.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const nameData = document.getElementById("name");
+const emailData = document.getElementById("email");
+const feedbackData = document.getElementById("feedback");
+const submitForm = document.getElementById("submit");
