62dd834659030fde2fbf86f19f9c0c54a6ca8c5f
diegosno <dserranoleal@gmail.com>
2023-04-17 21:50:10 +0200

    Handle email error

--- assets/js/form.js
+++ assets/js/form.js
@@ -8,21 +8,26 @@ const serviceId = "service_krm73sn";
 const templateId = "template_4pvzm1f";
 emailjs.init(publicKey);
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  submitForm.innerText = "Sending..";
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    submitForm.innerText = "Sending..";
+  const inputText = {
+    name: nameData.value,
+    email: emailData.value,
+    feedback: feedbackData.value,
+  };
 
-    const inputText ={
-        name: nameData.value,
-        email: emailData.value,
-        feedback: feedbackData.value,
-    };
+  emailjs.send(serviceId, templateId, inputText).then(
+    () => {
+      submitForm.innerText = "Message sent!";
+      nameData.value = "";
+      emailData.value = "";
+      feedbackData.value = "";
+    },
 
-
- emailjs.send(serviceId, templateId, inputText)
-        .then(() => {
-submitForm.innerText = 'Message sent!';
-            nameData.value='';
-            emailData.value='';
-            feedbackData.value='';
+    (error) => {
+      submitForm.innerText = "There was an error";
+    }
+  );
+});
